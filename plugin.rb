1d13893e0d29d00afdfdafd82222cfda6202f737
Richard <richard@communiteq.com>
2025-02-20 09:52:34 +0000

    Add suppress groups setting

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: discourse-groups-review-queue
-# version: 1.0
+# version: 1.0.1
 # authors: Communiteq
 # about: Show groups in review queue
 # url: https://www.github.com/communiteq/discourse-groups-review-queue
@@ -8,6 +8,7 @@ enabled_site_setting :groups_review_queue_enabled
 
 after_initialize do
   add_to_serializer(:UserWithCustomFields, :groups, include_condition: -> {  scope.user&.staff? }) do
-    object.groups.pluck(:name)
+    suppressed_group_ids = SiteSetting.groups_review_queue_suppressed_groups.split("|").map(&:to_i)
+    object.groups.where.not(id: suppressed_group_ids).pluck(:name)
   end
 end
